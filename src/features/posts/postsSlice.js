4049d409277a197853f853c9d75358d182d0ce47
diegopalones <diego.palones@gmail.com>
2023-01-02 16:00:33 +0100

    post service, slice update and comment post

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -32,42 +32,62 @@ export const getPostByName = createAsyncThunk(
   }
 );
 export const deletePost = createAsyncThunk(
-    "posts/deletePost",
-    async (id, thunkAPI) => {
-      try {
-        let action = await postsService.deletePost(id);
-        if (action.post == null) {
-          return thunkAPI.rejectWithValue(action);
-        }
-        return action;
-      } catch (error) {
-        const message = error.response.data;
-        return thunkAPI.rejectWithValue(message);
+  "posts/deletePost",
+  async (id, thunkAPI) => {
+    try {
+      let action = await postsService.deletePost(id);
+      if (action.post == null) {
+        return thunkAPI.rejectWithValue(action);
       }
-    }
-  );
-  export const like = createAsyncThunk("posts/like", async (_id) => {
-    try {
-      return await postsService.like(_id);
+      return action;
     } catch (error) {
-      console.error(error);
+      const message = error.response.data;
+      return thunkAPI.rejectWithValue(message);
     }
-  });
-  
-  export const unLike = createAsyncThunk("posts/like", async (_id) => {
+  }
+);
+export const like = createAsyncThunk("posts/like", async (_id) => {
+  try {
+    return await postsService.like(_id);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+export const unLike = createAsyncThunk("posts/like", async (_id) => {
+  try {
+    return await postsService.unLike(_id);
+  } catch (error) {
+    console.error(error);
+  }
+});
+export const createPost = createAsyncThunk("posts/", async (postData) => {
+  try {
+    return await postsService.createPost(postData);
+  } catch (error) {
+    console.error(error);
+  }
+});
+export const updatePost = createAsyncThunk(
+  "posts/update",
+  async (data, thunkAPI) => {
     try {
-      return await postsService.unLike(_id);
+      return await postsService.updatePost(data);
     } catch (error) {
-      console.error(error);
+      const message = error.response.data.message;
+      return thunkAPI.rejectWithValue(message);
+      // console.error(error)
     }
-  });
-  export const createPost = createAsyncThunk("posts/", async (postData) => {
-    try {
-      return await postsService.createPost(postData);
-    } catch (error) {
-      console.error(error);
-    }
-  });
+  }
+);
+
+export const comment = createAsyncThunk("posts/comments", async (comment) => {
+  try {
+    return await postsService.comment(comment);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 export const postsSlice = createSlice({
   name: "posts",
@@ -90,19 +110,20 @@ export const postsSlice = createSlice({
       })
       .addCase(getById.fulfilled, (state, action) => {
         state.post = action.payload.post;
+      });
+    builder
+      .addCase(getPostByName.fulfilled, (state, action) => {
+        state.posts = action.payload;
       })
-    builder.addCase(getPostByName.fulfilled, (state, action) => {
-      state.posts = action.payload;
-    })
-    .addCase(deletePost.fulfilled, (state, action) => {
+      .addCase(deletePost.fulfilled, (state, action) => {
         state.posts = state.posts.filter(
           (post) => post._id !== action.payload.post._id
         );
         state.isSuccess = true;
         state.isError = false;
         state.message = action.payload.message;
-    })
-    .addCase(deletePost.rejected, (state, action) => {
+      })
+      .addCase(deletePost.rejected, (state, action) => {
         state.isError = true;
         state.isSuccess = false;
         state.message = action.payload.message;
@@ -116,10 +137,40 @@ export const postsSlice = createSlice({
         });
         state.posts = posts;
       })
+      .addCase(unLike.fulfilled, (state, action) => {
+        const posts = state.posts.map((post) => {
+          if (post._id === action.payload._id) {
+            post = action.payload;
+          }
+          return post;
+        });
+        state.posts = posts;
+      })
       .addCase(createPost.fulfilled, (state, action) => {
         state.posts = [action.payload, ...state.posts];
       })
-    
+      .addCase(updatePost.fulfilled, (state, action) => {
+        const posts = state.posts.map((post) => {
+          if (post._id === action.payload.post._id) {
+            post = action.payload.post;
+          }
+          return post;
+        });
+        state.posts = posts;
+        state.isSuccess = true;
+        state.isError = false;
+        state.message = action.payload.message;
+      })
+      .addCase(updatePost.rejected, (state, action) => {
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.payload;
+      })
+      .addCase(comment.fulfilled, (state, action) => {
+        state.isSuccess = true;
+        state.message = action.payload.message;
+        state.post = action.payload;
+      });
   },
 });
 
