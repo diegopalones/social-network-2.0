0cfb0ac757d6ae40966ab05d96805bbf0a599802
diegopalones <diego.palones@gmail.com>
2023-01-02 15:30:53 +0100

    not found page added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import Home from './components/Home/Home';
-import Header from './components/Header/Header';
-import Profile from './components/Profile/Profile';
-import PostDetail from './components/PostDetail/PostDetail';
-import Search from './components/Search/Search';
-import Admin from './components/Admin/Admin';
-import PrivateZone from './guards/PrivateZone';
-import AdminZone from './guards/AdminZone';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Profile from "./components/Profile/Profile";
+import PostDetail from "./components/PostDetail/PostDetail";
+import Search from "./components/Search/Search";
+import Admin from "./components/Admin/Admin";
+import PrivateZone from "./guards/PrivateZone";
+import AdminZone from "./guards/AdminZone";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Header/>
-      <Routes>
+        <Header />
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
@@ -31,9 +32,16 @@ function App() {
           />
           <Route path="/post/id/:_id" element={<PostDetail />} />
           <Route path="/search/:postName" element={<Search />} />
-          <Route path="/admin" element={<AdminZone><Admin /></AdminZone>} />
-     
-      </Routes>
+          <Route
+            path="/admin"
+            element={
+              <AdminZone>
+                <Admin />
+              </AdminZone>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
