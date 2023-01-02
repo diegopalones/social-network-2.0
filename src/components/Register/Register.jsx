826f71357f1212d478440c793ce79a2c54d174d3
diegopalones <diego.palones@gmail.com>
2023-01-02 08:50:40 +0100

    register done

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("formData", formData);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" name="name" value={name} onChange={onChange} />
+      <input type="email" name="email" value={email} onChange={onChange} />
+
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={onChange}
+      />
+
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default Register;
